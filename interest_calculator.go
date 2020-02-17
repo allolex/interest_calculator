f3d3c565717c56e04a8316a5804496279c0c752e
Damon Davison <damon@curatur.com>
2020-02-17 11:31:04 -0500

    Initialize with working calculator

---
+++ interest_calculator.go
@@ -0,0 +1,46 @@
+//
+// intcal.go
+// Copyright (C) 2020 damon <damon@asheville>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	const MONTHLY_RATE float64 = 1.5
+
+	fmt.Println("Calculate interest on a late invoice.")
+	fmt.Println("This assumes", MONTHLY_RATE, "monthly interest.")
+
+	fmt.Println("\nEnter the number of days late: ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	days_late, _ := strconv.ParseFloat(scanner.Text(), 64)
+
+	if scanner.Err() != nil {
+		fmt.Println("There was an error")
+	}
+
+	fmt.Println("\nEnter the invoice amount: ")
+
+	amount_scanner := bufio.NewScanner(os.Stdin)
+	amount_scanner.Scan()
+	invoice_amount, _ := strconv.ParseFloat(amount_scanner.Text(), 64)
+
+	yearly_rate := MONTHLY_RATE * 12.0
+	yearly_multiplier := yearly_rate / 100.0
+	year_portion := days_late / 365.0
+	partial_rate := year_portion * yearly_multiplier
+	interest_amount := invoice_amount * partial_rate
+
+	fmt.Println("Interest Owed:", interest_amount)
+}
